bcb713a0ce8ce4018b8379cf074921e36f713c35
Sindy <sindyhuangdev@gmail.com>
2020-10-03 19:44:41 +0800

    vueTravel-v1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 Vue.config.productionTip = false
+fastClick.attach(document.body)
+
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <div>Ｈello world</div>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: "Home",
 };
 </script>
 
