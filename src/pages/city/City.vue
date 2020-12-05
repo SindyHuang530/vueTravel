d45b72a09044c31dbac04621cd35fddc593819fc
Sindy <sindyhuangdev@gmail.com>
2020-12-05 14:31:00 +0800

    add city-list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,16 +2,19 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 <script>
 import CityHeader from '../city/components/Header'
 import CitySearch from '../city/components/Search'
+import CityList from '../city/components/List'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   }
 }
 </script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title">當前City</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title">熱門City</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title">Ａ</div>
+      <div class="item-list">
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+      </div>
+      <div class="title">Ａ</div>
+      <div class="item-list">
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+      </div>
+      <div class="title">Ａ</div>
+      <div class="item-list">
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+        <div class="item">阿拉爾</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/styles/varibles';
+.area {
+  background: rgb(245, 243, 243);
+}
+.title {
+  color: rgb(143, 142, 142);
+  font-size: 1rem;
+  padding: 0.31rem 0.5rem;
+  border-bottom: solid 0.1rem rgb(221, 218, 218);
+}
+.button-list {
+  background: white;
+  overflow: hidden;
+  padding: 0.1rem 0.9rem 0.1rem 0.5rem;
+}
+.button-wrapper {
+  float: left;
+  margin: 0.25rem;
+  width: 31%;
+}
+.button {
+  border: 0.1rem solid rgb(221, 218, 218);
+  text-align: center;
+  border-radius: 0.2rem;
+  color: rgb(143, 142, 142);
+}
+.item {
+  color: rgb(143, 142, 142);
+  background: white;
+  line-height: 2rem;
+  padding-left: 0.5rem;
+  border-bottom: solid 0.1rem rgb(237, 232, 232);
+}
+.list {
+  overflow: hidden;
+  position: absolute;
+  top: 4.2rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
